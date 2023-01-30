5f6b182c3508e26aeca590eb6743124e65e4e49f
unknown <sunthanhan1308@gmail.com>
2023-01-30 20:07:18 +0700

    scroll snap type

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+export default function App() {
+	return (
+		<>
+			<section>
+				<h1>1</h1>
+			</section>
+			<section>
+				<h1>2</h1>
+			</section>
+			<section>
+				<h1>3</h1>
+			</section>
+			<section>
+				<h1>4</h1>
+			</section>
+			<section>
+				<h1>5</h1>
+			</section>
+		</>
+	)
 }
-
-export default App;
