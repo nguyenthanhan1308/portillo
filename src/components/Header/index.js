2d97a239d5cd95bc5b346d749748aaee84693dcf
unknown <sunthanhan1308@gmail.com>
2023-03-13 20:41:14 +0700

    nav container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,24 @@
+import {useState} from "react";
 import "./Header.css";
-import { Icon } from '@mui/material';
 export default function Header () {
+    const [isOpenNav, setIsOpenNav] = useState(false);
+    
+    const onOpenNav = () => setIsOpenNav(true);
+    const onCloseNav = () => setIsOpenNav(false);
+
+    
     const screenWidth = window.innerWidth;
+
     return (
         <div className="display_row full_w">
             <div className="header-container display_row full_w">
                 <img src="logo.png" alt="" />
-                {!(screenWidth < 768) ?
-                    <div className="header-nav gap2 display_row">
+                    <div className={`header-nav gap2 ${screenWidth <= 768 ? "display_col nav-container" : "display_row"}`}>
                         <a className="nav-link" href="about">About</a>
                         <a className="nav-link" href="contact">Contact</a>
                         <a className="nav-link" href="cv">CV</a>
                         <a className="nav-link" href="projects">Projects</a>
-                    </div> :
-                    <Icon
-                        component=""
-                    />
-                }
+                    </div>
             </div>
         </div>
     )
