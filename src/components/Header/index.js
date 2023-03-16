1983c5c0610acab227b5baf320e82262ca9faa98
unknown <sunthanhan1308@gmail.com>
2023-03-16 19:21:38 +0700

    container queries

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,16 @@
-import {useState} from "react";
+// import {useState} from "react";
 import "./Header.css";
 export default function Header () {
-    const [isOpenNav, setIsOpenNav] = useState(false);
+    // const [isOpenNav, setIsOpenNav] = useState(false);
     
-    const onOpenNav = () => setIsOpenNav(true);
-    const onCloseNav = () => setIsOpenNav(false);
-
+    // const onOpenNav = () => setIsOpenNav(true);
+    // const onCloseNav = () => setIsOpenNav(false);
     
-    const screenWidth = window.innerWidth;
-
     return (
         <div className="display_row full_w">
             <div className="header-container display_row full_w">
                 <img src="logo.png" alt="" />
-                    <div className={`header-nav gap2 ${screenWidth <= 768 ? "display_col nav-container" : "display_row"}`}>
+                    <div className={`header-nav gap2 nav-container`}>
                         <a className="nav-link" href="about">About</a>
                         <a className="nav-link" href="contact">Contact</a>
                         <a className="nav-link" href="cv">CV</a>
