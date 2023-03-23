76b987b264ff54978b995e2949321c41a8e02eac
unknown <sunthanhan1308@gmail.com>
2023-03-23 21:17:34 +0700

    nav sidebar responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,26 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./Header.css";
-import {MenuOutlined} from "@ant-design/icons"
-export default function Header () {
-    const windowWidth =  window?.innerWidth;
-    const [isOpenNav, setIsOpenNav] = useState(!!(windowWidth > 768));
-    
-    const onOpenNav = () => setIsOpenNav(true);
-    const onCloseNav = () => setIsOpenNav(false);
-    
+import { MenuOutlined } from "@ant-design/icons"
+export default function Header() {
+    const [isOpenNav, setIsOpenNav] = useState();
+
+    const toggleNav = () => setIsOpenNav(!isOpenNav);
+
     return (
         <div className="display_row full_w">
             <div className="header-container display_row full_w">
                 <img src="logo.png" alt="" />
-                    {!isOpenNav && <button className="nav-button" onClick={onOpenNav}>
-                            <MenuOutlined/>
-                        </button>}
-                    {isOpenNav &&
-                        <div className={`header-nav gap2 nav-container`}>
-                            <button className="nav-button" onClick={onCloseNav}>
-                                <MenuOutlined />
-                            </button>
-                            <a className="nav-link" href="#about">About</a>
-                            <a className="nav-link" href="contact">Contact</a>
-                            <a className="nav-link" href="cv">CV</a>
-                            <a className="nav-link" href="#projects">Projects</a>
-                        </div>
-                    }
+                <div className={`header-nav display_col`}>
+                    <button type="button" className="nav-button" onClick={toggleNav}>
+                        <MenuOutlined />
+                    </button>
+                    <div className={`gap2 nav-container ${isOpenNav ? "nav-open" : "nav-close"}`}>
+                        <a className="nav-link" href="#about">About</a>
+                        <a className="nav-link" href="contact">Contact</a>
+                        <a className="nav-link" href="cv">CV</a>
+                        <a className="nav-link" href="#projects">Projects</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
